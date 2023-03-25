3b319d7ccdb8c60c737c91bfccfa4d8aad3ce069
Vincent-Zhang4135 <65748334+Vincent-Zhang4135@users.noreply.github.com>
2023-03-25 17:52:53 -0500

    pusssiessss

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import { Canvas } from "@react-three/fiber";
 import Item from './Item';
 
 function App() {
+    const levels = [[-2.4, 0, 0], [-1.2, 0, 0], [0, 0, 0], [1.2, 0, 0], [2.4, 0, 0]];
+
     return (
         <header className="App-header">
             <Canvas>
                 <pointLight position={[10, 10, 10]} />
-                <Item position={[-1.2, 0, 0]} />
-                <Item position={[1.2, 0, 0]} />
+                { levels.map((level) => <Item position={[level[0], level[1], level[2]]} />) }
             </Canvas>
         </header>
     );
